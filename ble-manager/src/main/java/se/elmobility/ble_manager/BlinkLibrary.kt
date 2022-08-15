8544112053f989de04891d48e677506b602370c8
Albin Axtelius <albin@axtelius.se>
2022-08-15 16:47:38 +0200

    Initial commit

---
+++ ble-manager/src/main/java/se/elmobility/ble_manager/BlinkLibrary.kt
@@ -0,0 +1,11 @@
+package se.elmobility.ble_manager
+
+class FunClass {
+    fun hello() {
+        println("FunClass said: $MESSAGE")
+    }
+
+    companion object {
+        const val MESSAGE = "Hello world"
+    }
+}
